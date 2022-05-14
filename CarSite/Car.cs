dad673cc1b37023b7197ac9b3af60bb70037441e
Aphar Magaramova <77408835+ApharMagaramova@users.noreply.github.com>
2022-05-14 16:17:03 +0400

    Add files via upload

---
+++ CarSite/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CarSite
+{
+    class Car
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+
+        public bool Id { get; set; }
+        static int nextId;
+        public int ModelId { get; set; }
+
+        public int Year { get; set; }
+        public double Price { get; set; }
+        public string ban { get; set; }
+        public string color { get; set; }
+        public string yanacaq { get; set; }
+
+        enum Ban
+        {
+            hibrid, sedan
+        }
+
+        enum Color
+        {
+            ag = 1, qara = 2, qirmizi = 3, goy = 4
+        }
+
+        enum Yanacaq
+        {
+            dizel, benzin
+        }
+
+        public Car()
+        {
+            ModelId= Interlocked.Increment(ref nextId);
+        }
+
+        public Car(string marka, string model, int year, double price, int ban, int color, int yanacaq): this()
+        {
+            Marka = marka;
+            Model = model;
+            //ModelId = modelId;
+            Year = year;
+            Price = price;
+            if (ban == 1)
+                this.ban = Convert.ToString(Ban.hibrid);
+            if (ban == 2)
+                this.ban = Convert.ToString(Ban.sedan);
+
+            if (color == 1)
+                this.color = Convert.ToString(Color.ag);
+            if (color == 2)
+                this.color = Convert.ToString(Color.qara);
+            if (color == 3)
+                this.color = Convert.ToString(Color.qirmizi);
+            if (color == 4)
+                this.color = Convert.ToString(Color.goy);
+
+            if (yanacaq == 1)
+                this.yanacaq = Convert.ToString(Yanacaq.dizel);
+            if (yanacaq == 2)
+                this.yanacaq = Convert.ToString(Yanacaq.benzin);
+            
+        }
+
+        public override string ToString()
+        {
+            return Marka.ToString() + "\n" + Model.ToString() + "\n" + ModelId.ToString() + "\t" + Year.ToString() + "\t" +
+                Price.ToString() + "\t" + ban.ToString() + "\t" + color.ToString() + yanacaq.ToString();
+        }
+    }
+}
---
+++ CarSite/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CarSite
+{
+    class Program
+    {
+        static int _key;
+        static void Main(string[] args)
+        {
+            IDictionary<int, Car> car = new Dictionary<int, Car>();
+
+            while (true)
+            {
+                Menu();
+                Console.WriteLine();
+                switch (Console.ReadLine())
+                {
+
+                    case "1":
+                        foreach (var item in car)
+                        {
+                            Console.WriteLine(item.Value.ToString());
+                        }
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter marka");
+                        string marka = Console.ReadLine();
+
+                        Console.WriteLine("enter Model");
+                        string model = Console.ReadLine();
+
+                        Console.WriteLine("enter year");
+                        int year = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("enter price");
+                        double price = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("enter ban");
+                        int ban = Convert.ToInt32(Console.ReadLine());
+                        
+                        Console.WriteLine("enter color number");
+                        int color = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Enter yanacaq");
+                        int yanacaq = Convert.ToInt32(Console.ReadLine());
+
+                        int key = Interlocked.Increment(ref _key);
+
+                        car.Add(key, new Car(marka, model, year, price, ban, color, yanacaq));
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter the id of the car you want to remove");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        car.Remove(id);
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter the id of the car you want to update");
+                        int _id = Convert.ToInt32(Console.ReadLine());
+
+                        break;
+                    default:
+                        Console.WriteLine("Bele bir emeliyyat yoxdur");
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("Əməliyyatlar");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("1.Kataloq ");
+            Console.WriteLine("2.Elan yerləşdirmək");
+            Console.WriteLine("3.Elan silmək");
+            Console.WriteLine("4.Redaktə et");
+            Console.WriteLine("5.Çıxış");
+            Console.WriteLine("6.Save");
+        }
+    }
+}
